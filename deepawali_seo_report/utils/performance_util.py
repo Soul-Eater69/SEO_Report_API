69da3bc32a47393a2f4ecf2d484cf7a4f23fdf6d
Soul-Eater69 <51698530+Soul-Eater69@users.noreply.github.com>
2023-03-26 21:14:15 +0530

    Update performance_util.py

--- deepawali_seo_report/utils/performance_util.py
+++ deepawali_seo_report/utils/performance_util.py
@@ -5,8 +5,6 @@ import ssl
 from bs4 import BeautifulSoup
 
 
-
-
 class PerformanceUtil:
 
     def __new__(cls, *args, **kwargs):
@@ -17,8 +15,39 @@ class PerformanceUtil:
     def __init__(self, soup_obj):
         self.url = soup_obj["url"]
         self.response = soup_obj["response"]
-        self.soup = BeautifulSoup(self.response["text"],"html.parser")
+        self.soup = BeautifulSoup(self.response["text"], "html.parser")
         self.data = soup_obj["desktop_data"]
+
+    def get_response_times(self):
+        items = self.data["lighthouseResult"]["audits"]["network-requests"]["details"]["items"]
+        # Initialize variables to store the load times
+        server_response_time = self.data["lighthouseResult"]["audits"][
+            "server-response-time"]["details"]["items"][0]["responseTime"]
+        content_load_time = 0
+        script_load_time = 0
+
+        # Loop through each network request and calculate the load times
+        for item in items:
+            try:
+                code = item["statusCode"]
+                if code == 200:
+                    mimeType = item["mimeType"]
+                    requestDuration = item["networkEndTime"] - item["networkRequestTime"]
+                    resourceType = item["resourceType"]
+                
+                    if ("javascript" in mimeType) or (resourceType=="Script"):
+                        script_load_time += requestDuration
+                    else:
+                        content_load_time += requestDuration
+            except Exception as e:
+                print(item)
+                print(e)
+                continue
+
+        return {"server_response_time": server_response_time/1000, 
+                "content_load_time": content_load_time//1000, 
+                "script_load_time": script_load_time//1000
+                }
 
     def get_compression_data(self):
         resources = self.data["lighthouseResult"]["audits"]["network-requests"]["details"]["items"]
@@ -34,19 +63,20 @@ class PerformanceUtil:
         resource_sizes = {
             "html": {"original": 0, "compressed": 0, "rate": 0},
             "css": {"original": 0, "compressed": 0, "rate": 0},
-            "javascript": {"original": 0, "compressed": 0, "rate": 0},
-            "image": {"original": 0, "compressed": 0, "rate": 0},
+            "js": {"original": 0, "compressed": 0, "rate": 0},
+            "images": {"original": 0, "compressed": 0, "rate": 0},
             "other": {"original": 0, "compressed": 0, "rate": 0},
             "total_size": 0,
             "total_compressed_size": 0,
+            "total_rate": 0,
         }
 
         # Loop through resources and add up sizes for each type
         for resource in resources:
             try:
                 content_type = resource["mimeType"]
-                size = resource["resourceSize"]/ 1_000_000
-                compressed_size = resource["transferSize"]/ 1_000_000
+                size = resource["resourceSize"] / 1_000_000
+                compressed_size = resource["transferSize"] / 1_000_000
                 obj_counts["total_objects"] += 1
 
                 if "text/html" in content_type:
@@ -60,13 +90,13 @@ class PerformanceUtil:
                     obj_counts["css_obj"] += 1
 
                 elif "javascript" in content_type:
-                    resource_sizes["javascript"]["original"] += size
-                    resource_sizes["javascript"]["compressed"] += compressed_size
+                    resource_sizes["js"]["original"] += size
+                    resource_sizes["js"]["compressed"] += compressed_size
                     obj_counts["javascript_obj"] += 1
 
                 elif "image/" in content_type:
-                    resource_sizes["image"]["original"] += size
-                    resource_sizes["image"]["compressed"] += compressed_size
+                    resource_sizes["images"]["original"] += size
+                    resource_sizes["images"]["compressed"] += compressed_size
                     obj_counts["image_obj"] += 1
 
                 else:
@@ -89,6 +119,13 @@ class PerformanceUtil:
                         1 - (sizes["compressed"] / sizes["original"])) * 100
                     sizes["rate"] = compression_ratio
 
+        resource_sizes["total_rate"] = (
+            resource_sizes["total_compressed_size"]/resource_sizes["total_size"])*100
+
+        for i in resource_sizes:
+            if isinstance(resource_sizes[i], dict) and resource_sizes[i]["rate"] < 0:
+                resource_sizes[i]["rate"] = 0
+
         return {"counts": obj_counts, "sizes": resource_sizes}
 
     def get_amp_data(self):
@@ -98,7 +135,7 @@ class PerformanceUtil:
             "amp_css_boilerplate": False,
             "custom_css": False,
             "canonical_link": False,
-            "amp_images": False
+            "amp_images": False,
         }
 
         # Check for the AMP runtime script
@@ -135,26 +172,9 @@ class PerformanceUtil:
             amp_data['amp_images'] = True
 
         return amp_data
-    
-    def get_js_errors(self):
-        # Create a new Chrome browser instance
-        options = webdriver.ChromeOptions()
-        options.add_argument('headless')
-        driver = webdriver.Chrome(executable_path="D:\\driver\\chromedriver.exe",options=options)
 
-        # Navigate to the website
-        driver.get(self.url)
-
-        error_strings = ["SyntaxError", "EvalError", "ReferenceError", "RangeError", "TypeError", "URIError"]
-        js_errors = driver.get_log("browser")
-        js_errors = [error for error in js_errors if any(error_string in error["message"] for error_string in error_strings)]
-        
-        driver.quit()
-        if js_errors:
-            return True
-        
-        return False
     
+
     def check_http2(self):
         try:
             HOST = urlparse(self.url).netloc
@@ -175,35 +195,34 @@ class PerformanceUtil:
         except Exception as e:
             print(e)
             return {"http2": False}
-    
+
     def check_optimized_images(self):
         audits = self.data['lighthouseResult']['audits']
         image_optimization_audit = audits['uses-optimized-images']
-        
+
         if image_optimization_audit['score'] == 1:
-            return {"optimized":True}
+            return True
         else:
-            return {"optimized":False}
-    
+            return False
+
     def check_minified_files(self):
         audits = self.data['lighthouseResult']['audits']
         css_minification_audit = audits['unminified-css']
         js_minification_audit = audits['unminified-javascript']
 
-        result = {
-            "css":css_minification_audit['score'] == 0,
-            "javascript":js_minification_audit['score'] == 0
-        }
+        print("\n\n",css_minification_audit,js_minification_audit,"\n\n")
 
-        return result
-    
+        if css_minification_audit['score'] == 1 or js_minification_audit['score'] == 1:
+            return True
+        return False
+
     def check_depricated_html(self):
         obsolete_tags = self.soup.find_all(['center', 'font', 'marquee'])
-    
+
         if len(obsolete_tags) == 0:
-            return {"depricated-html":False}
+            return False
         else:
-            return {"depricated-html":True}
+            return True
 
     def check_inline_styles(self):
         elements = self.soup.find_all(style=True)
@@ -212,6 +231,6 @@ class PerformanceUtil:
             styles = el['style'].strip()
             lineno = el.sourceline
 
-            res.append({"styles":styles, "lineno":lineno})
+            res.append({"styles": styles, "lineno": lineno})
 
-        return {"inline_styles": res}
+        return {"inline_styles": res, "hasStyles":True if len(res)>0 else False}
