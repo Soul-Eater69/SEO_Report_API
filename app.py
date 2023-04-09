2089f8173a46270777c439cf20c2b0f0e697b021
Mahesh Changal <mahesh.changal@tigeranalytics.local>
2023-04-09 17:54:29 +0530

    added api new

--- app.py
+++ app.py
@@ -9,7 +9,11 @@ api = Api(app)
 api.add_resource(OnPageSEO, "/getOnPageSEOReport")
 api.add_resource(Usability, "/getUsabilityReport")
 api.add_resource(Performance, "/getPerformanceReport")
+api.add_resource(Social, "/getSocialReport")
 api.add_resource(Setup, "/setup")
+api.add_resource(Login, "/login")
+api.add_resource(Users, "/user")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
