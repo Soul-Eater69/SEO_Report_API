fe799920b1e4618f1ad6d2f2ca00dafcfa42b820
Soul-Eater69 <51698530+Soul-Eater69@users.noreply.github.com>
2023-04-11 21:59:16 +0530

    Update __init__.py

--- deepawali_seo_report/__init__.py
+++ deepawali_seo_report/__init__.py
@@ -1,20 +1,21 @@
 from flask import Flask
 from flask_cors import CORS
 from .database.db import initialize_db
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 app = Flask(__name__)
+app.config.from_prefixed_env()
 cors = CORS(app)
 
-app.config['SECRET_KEY'] = "SECRET_KEY" 
+app.config['SECRET_KEY'] = app.config["SECRET_KEY"]
 
 
-app.config['MONGODB_SETTINGS'] = {
-    "db": "deepawali",
-    "host": "mongodb://mongo:CA5yXPtFhNLa9bgiTbj7@containers-us-west-58.railway.app:6245/deepawali",
-    "username": "mongo",
-    "password": "CA5yXPtFhNLa9bgiTbj7",
-    "port": 6245
-}
+DB_URI = app.config["DB_URI"]
+print(DB_URI)
+app.config["MONGODB_HOST"] = DB_URI
 
 initialize_db(app)
